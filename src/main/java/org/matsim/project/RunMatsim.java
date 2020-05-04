33f542b9e1f99105538310a0f3ea7b87d65bf7b8
danielundgit <jaramilloplaumann@campus.tu-berlin.de>
2020-05-04 18:50:02 +0200

    config: iteration to 2 network: capacity change runmatsim: remove and add persons/plans

--- src/main/java/org/matsim/project/RunMatsim.java
+++ src/main/java/org/matsim/project/RunMatsim.java
@@ -18,11 +18,20 @@
  * *********************************************************************** */
 package org.matsim.project;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.*;
+import org.matsim.contrib.otfvis.OTFVisLiveModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.prefs.Preferences;
 
 /**
  * @author nagel
@@ -33,7 +42,17 @@ public class RunMatsim{
 	public static void main(String[] args) {
 
 		Config config = ConfigUtils.loadConfig( args ) ;
-		
+
+		if ( args==null || args.length==0 || args[0]==null ){
+			config = ConfigUtils.loadConfig( "scenarios/equil/config.xml" );
+		} else {
+			config = ConfigUtils.loadConfig( args );
+		}
+		config.controler().setOverwriteFileSetting(
+				OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
+
+
+
 		// possibly modify config here
 		
 		// ---
@@ -41,10 +60,51 @@ public class RunMatsim{
 		Scenario scenario = ScenarioUtils.loadScenario(config) ;
 		
 		// possibly modify scenario here
-		
+		Id<Person> carol = Id.createPersonId(1);
+		List<Id<Person>> personsToRemove = new ArrayList<>();
+
+		for(Id<Person> personId : scenario.getPopulation().getPersons().keySet()){
+			if(!personId.equals(carol)){
+				personsToRemove.add(personId);
+			}
+		}
+
+		for (Id<Person> personId : personsToRemove){
+			scenario.getPopulation().removePerson(personId);
+		}
+
+		System.out.println("Population size = " + scenario.getPopulation().getPersons().size());
+
+		// ---
+
+		PopulationFactory populationFactory = scenario.getPopulation().getFactory();
+
+		Person person2 = populationFactory.createPerson(Id.createPersonId("Daniel"));
+
+		Plan plan = populationFactory.createPlan();
+
+		Activity homeActivity = populationFactory.createActivityFromLinkId("h", Id.createLinkId(21));
+		homeActivity.setEndTime(6*60*60.);
+		plan.addActivity(homeActivity);
+
+		Leg leg = populationFactory.createLeg(TransportMode.car);
+		plan.addLeg(leg);
+
+		Activity workActivity = populationFactory.createActivityFromLinkId("w", Id.createLinkId(1));
+		plan.addActivity(workActivity);
+
+		person2.addPlan(plan);
+
+		scenario.getPopulation().addPerson(person2);
+
+		System.out.println("Population size = " + scenario.getPopulation().getPersons().size());
+
 		// ---
 		
 		Controler controler = new Controler( scenario ) ;
+
+		controler.addOverridingModule( new OTFVisLiveModule() ) ;
+		controler.run();
 		
 		// possibly modify controler here
 
